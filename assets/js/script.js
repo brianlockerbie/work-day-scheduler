eb7908dbcf07d33352091389ff3e9714634b03cf
Brian <brian.lockerbie@gmail.com>
2022-03-25 16:31:14 -0700

    added classes & ids to time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
+var savedItems = {};
+
 var today = moment().format("dddd, MMMM Do");
+
+var currentTime = parseInt(moment().format('H'))
 
 $("#currentDay")
     .text(today);
+
+
+var createItem = function () {
+    var text = $(this)
+        .text()
+        .trim()
+
+    var textInput = $("<textarea")
+        .addClass("m-0")
+        .val(text)
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+}
+
 
 $(".textBox").on("click", function(){
     var text = $(this)
