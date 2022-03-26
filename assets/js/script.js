28407a19d3f1d5042317adc9657118e0708a6604
Brian <brian.lockerbie@gmail.com>
2022-03-25 20:02:22 -0700

    added js and more css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var savedItems = {};
 var today = moment().format("dddd, MMMM Do");
 
 var currentTime = parseInt(moment().format('H'))
+console.log(currentTime);
 
 $("#currentDay")
     .text(today);
@@ -22,10 +23,97 @@ var createItem = function () {
 }
 
 
-$(".textBox").on("click", function(){
+$(".textBox").on("click", "p", createItem)
+
+var confirmItem = function(text, id){
     var text = $(this)
-        .text()
+        .val()
         .trim()
 
-        console.log(text);
-})
+    var id = $(this)
+        .closest(".row")
+        .attr("id")
+
+    var pReturn = $("<p>")
+        .attr("data-time", id)
+        .addClass("my-auto h-75 w-100 pt-3")
+        .text(text);
+
+    $(this).replaceWith(pReturn);
+
+}
+
+$(".textBox").on("blur", "textarea", confirmItem)
+
+var saveButton = function(event) {
+
+    console.log(savedItems);
+
+    var selectedParent =$(event.target)
+        .closest(".row")
+
+    var selectedTime = selectedParent
+        .attr("id")
+
+    var selectedText = selectedParent
+        .children(".textBox")
+        .children("p")
+        .text()
+    
+    savedItems[selectedTime] = selectedText
+    console.log(savedItems)
+
+    saveItem(savedItems);
+
+}
+
+var saveItem = function () {
+    localStorage.setItem("items", JSON.stringify(savedItems))
+}
+
+var loadItems = function () {
+    savedItems = JSON.parse(localStorage.getItem("items"))
+
+    if(!savedItems) {
+        savedItems = {};
+    }
+    
+    $.each(savedItems, function(list, arr) {
+
+        var test = $("#" + list)
+            .find(".textBox")
+            .children("p")
+            .text(arr);
+
+    })
+}
+
+$(".saveBtn").on("click", saveItem);
+
+loadItems();
+
+var timeItems = function () {
+    var selection = $(".row").each(function() {
+        
+        var theTime = parseInt($(this)
+            .attr("id"))
+
+        if(currentTime > theTime) {       
+            check = $(this)
+                .find(".textBox")
+                .addClass("past-item")
+        } else if (currentTime < theTime) {
+            check = $(this)
+                .find(".textBox")
+                .addClass("future-item")
+        } else if (currentTime === theTime) {
+            check = $(this)
+            .find(".textBox")
+            .addClass("current-item")
+        }
+    })
+}
+
+timeItems();
+
+setInterval(timeItems, (60*1000)*30)
