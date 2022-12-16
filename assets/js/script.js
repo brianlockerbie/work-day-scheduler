63f5ec0a10e67f59f97e7eb1f6a1810dcbbbcfae
Brian <brian.lockerbie@gmail.com>
2022-12-16 15:04:24 -0800

    cleaning up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,44 @@
-var displayTime = document.querySelector("#currentDay");
-var currentTime = moment();
+var savedItems = {};
 
-displayTime.textContent = currentTime.format("dddd, MMMM Do")
-
-$(".saveBtn").on("click", function() {
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    // save in localStorage
-    localStorage.setItem(time /* aka KEY = row id */, value /* text in the textarea */);
-});
-
+var today = moment().format("dddd, MMMM Do");
 var currentTime = parseInt(moment().format('H'))
 
-// 9AM
-$("#9am-row .description").val(localStorage.getItem("9am-row" /* localStorage KEY also the row id */));
 
-// 10am
-$("#10am-row .description").val(localStorage.getItem("#10am-row"));
 
-// 11am
-$("#11am-row .description").val(localStorage.getItem("11am-row"));
-    
-// 12pm
-$("#12pm-row .description").val(localStorage.getItem("12pm-row"));
-    
-// 1pm
-$("#1pm-row .description").val(localStorage.getItem("1pm-row"));
-    
-// 2pm
-$("#2pm-row .description").val(localStorage.getItem("2pm-row"));
-    
-// 3pm
-$("#3pm-row .description").val(localStorage.getItem("3pm-row"));
-    
-// 4pm
-$("#4pm-row .description").val(localStorage.getItem("4pm-row"));
-    
-// 5pm
-$("#5pm-row .description").val(localStorage.getItem("5pm-row"));
-    
 
-var hour9 = 9;
-var hour10 = 10;
-var hour11 = 11;
-var hour12 = 12;
-var hour1 = 13;
-var hour2 = 14;
-var hour3 = 15;
-var hour4 = 16;
-var hour5 = 17;
+$("#currentDay")
+    .text(today);
 
-var currentHour = moment().format('H');
-    
-// 9am 
-if (currentHour < hour9) {
-    $("#9am-text").addClass("future");
-} else if (currentHour > hour9) {
-    $("#9am-text").addClass("past");
-} else if (currentHour = hour9) {
-    $("#9am-text").addClass("present");
+
+var createItem = function () {
+    var text = $(this)
+        .text()
+        .trim()
+
+    var textInput = $("<textarea>")
+        .addClass("m-0")
+        .val(text)
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
 }
     
-// 10am 
-if (currentHour < hour10) {
-    $("#10am-text").addClass("future");
-} else if (currentHour > hour10) {
-    $("#10am-text").addClass("past");
-} else if (currentHour = hour10) {
-    $("#10am-text").addClass("present");
-}
+$(".textBox").on("click", "p", createItem)
 
-// 11am
-if (currentHour < hour11) {
-    $("#11am-text").addClass("future");
-} else if (currentHour > hour11) {
-    $("#11am-text").addClass("past");
-} else if (currentHour = hour11) {
-    $("#11am-text").addClass("present");
-}
 
-// 12pm
-if (currentHour < hour12) {
-    $("#12pm-text").addClass("future");
-} else if (currentHour > hour12) {
-    $("#12pm-text").addClass("past");
-} else if (currentHour = hour12) {
-    $("#12pm-text").addClass("present");
-}
+var confirmItem = function(text, id) {
+    var text = $(this)
+        .val()
+        .trim()
 
-// 1pm
-if (currentHour < hour1) {
-    $("#1pm-text").addClass("future");
-} else if (currentHour > hour1) {
-    $("#1pm-text").addClass("past");
-} else if (currentHour = hour1) {
-    $("#1pm-text").addClass("present");
-}
+    var id = $(this)
+        .closest(".row")
+        .attr("id")
 
-// 2pm
-if (currentHour < hour2) {
-    $("#2pm-text").addClass("future");
-} else if (currentHour > hour2) {
-    $("#2pm-text").addClass("past");
-} else if (currentHour = hour2) {
-    $("#2pm-text").addClass("present");
-}
+    var pReturn = $("<p>")
+        attr("data-time", id)
+        .addClass("my-auto h-75 w-100 pt-3")
+        .text(text);
 
-// 3pm
-if (currentHour < hour3) {
-    $("#3pm-text").addClass("future");
-} else if (currentHour > hour3) {
-    $("#3pm-text").addClass("past");
-} else if (currentHour = hour3) {
-    $("#3pm-text").addClass("present");
-}
-
-// 4pm
-if (currentHour < hour4) {
-    $("#4pm-text").addClass("future");
-} else if (currentHour > hour4) {
-    $("#4pm-text").addClass("past");
-} else if (currentHour = hour4) {
-    $("#4pm-text").addClass("present");
-}
-
-// 5pm
-if (currentHour < hour5) {
-    $("#5pm-text").addClass("future");
-} else if (currentHour > hour5) {
-    $("#5pm-text").addClass("past");
-} else if (currentHour = hour5) {
-    $("#5pm-text").addClass("present");
+    $(this).replaceWith(pReturn);
 }
